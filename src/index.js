132bcac8ddf8add5a9343912d85518d41ded30d2
porwalsarvagya09 <porwalsarvagya09@gmail.com>
2025-07-15 18:17:53 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -9,25 +9,14 @@ dotenv.config({
 
 connectDB()
 
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed", error);
+})
 
 
 
-
-/*;( async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error;
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("Error", error)
-    }
-} )()
-
-
-*/
